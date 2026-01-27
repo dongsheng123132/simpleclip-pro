7bc9c1b443beafd3f7874a58577f8dcfd6030d76
hfshfg <38004547@qq.com>
2026-01-27 22:36:42 +0800

    Initial Commit

---
+++ SimpleClip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleClip
+//
+//  Created by dosen8618607105700 on 2026/1/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleClip/SimpleClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleClipApp.swift
+//  SimpleClip
+//
+//  Created by dosen8618607105700 on 2026/1/27.
+//
+
+import SwiftUI
+
+@main
+struct SimpleClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
